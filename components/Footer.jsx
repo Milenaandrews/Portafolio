428f0e4b1bcf85fe912ed919406a2fb412deada5
Milenaandrews <mileandrews@gmail.com>
2024-02-13 13:51:05 -0300

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,13 +5,6 @@ const Footer = () => {
     <div className='flex flex-col flex-nowrap justify-center content-center gap-3 pb-40'>
         <div className='flex justify-center' >
             <h3>Portafolio 2024</h3>
-            <ul>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
-            </ul>
         </div>
         <div className='flex justify-center'>
             {/* <p>copyright &copy;2024 designed by <span>Milenaria</span></p> */}
